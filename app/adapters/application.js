be657d7d8f0cbbb009d20a5cdc6f820abddd1943
palimpsests <zach.zibrat@locusenergy.com>
2015-01-24 18:49:27 -0800

    removes comment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,6 @@
 import DS from "ember-data";
 import config from '../config/environment';
 
-console.log('config: ', config)
-
 export default DS.RESTAdapter.extend({
   namespace: 'api',
   host: config.APP.API_HOST
