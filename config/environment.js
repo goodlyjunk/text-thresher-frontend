91ddedea5eca52a8fe1bcc9a574115f9b1f5f6da
palimpsests <zach.zibrat@locusenergy.com>
2015-04-27 21:17:48 -0700

    fixes flexbox

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.APP.API_HOST = 'http://127.0.0.1:5000';
+    ENV.APP.API_HOST = 'https://text-thresher.herokuapp.com';
   }
 
   if (environment === 'test') {
